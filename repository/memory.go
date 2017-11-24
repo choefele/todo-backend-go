ba1079569a8de95a2dd2dbac2d053a1b388b942c
Claus Höfele <claus@claushoefele.com>
2017-11-24 17:04:17 +0100

    Use mutext for thread-safety

--- repository/memory.go
+++ repository/memory.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"sync"
 
 	"github.com/choefele/todo-backend-go/service"
 )
 
 type memoryService struct {
+	mtx   sync.RWMutex
 	id    int
 	list  []*service.Todo
 	index map[string]*service.Todo
@@ -22,6 +24,9 @@ func NewMemoryService() service.TodoService {
 }
 
 func (m *memoryService) Create(_ context.Context, form service.TodoForm) (service.Todo, error) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
 	todo := service.Todo{
 		ID:    strconv.Itoa(m.id),
 		Title: form.Title,
@@ -33,6 +38,9 @@ func (m *memoryService) Create(_ context.Context, form service.TodoForm) (servic
 }
 
 func (m *memoryService) Todo(_ context.Context, id string) (service.Todo, error) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
 	todo, ok := m.index[id]
 	if !ok {
 		return service.Todo{}, fmt.Errorf("Invalid index `%v`", id)
@@ -42,6 +50,9 @@ func (m *memoryService) Todo(_ context.Context, id string) (service.Todo, error)
 }
 
 func (m *memoryService) Todos(_ context.Context) ([]service.Todo, error) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
 	res := []service.Todo{}
 	for _, todo := range m.list {
 		res = append(res, *todo)
