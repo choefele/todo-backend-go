be46af4c1e38e1aa979b53a6a87f171f922cc85e
Claus Höfele <claus@claushoefele.com>
2017-11-27 14:26:36 +0100

    Simplify HTTP tests
    Test without serving handler

--- transport/http_test.go
+++ transport/http_test.go
@@ -12,11 +12,10 @@ import (
 func TestTodos(t *testing.T) {
 	todoService := repository.NewMemoryService()
 	server := NewHTTPServer(todoService)
-	handler := http.HandlerFunc(server.todos)
 
 	req, _ := http.NewRequest("GET", "", nil)
 	w := httptest.NewRecorder()
-	handler.ServeHTTP(w, req)
+	server.todos(w, req)
 
 	if w.Code != http.StatusOK {
 		t.Errorf("Should return %v but was %v", http.StatusOK, w.Code)
