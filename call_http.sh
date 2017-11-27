09754be8eec579cb51703cf3ee7e942f4fcce2a6
Claus Höfele <claus@claushoefele.com>
2017-11-27 10:50:30 +0100

    Add handler to create todo

--- call_http.sh
+++ call_http.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+curl -X POST -d "{\"test\": \"that\"}" localhost:8080/api/todo
+echo
 curl localhost:8080/api/todos
+echo
--- transport/http.go
+++ transport/http.go
@@ -23,18 +23,40 @@ func (h *HTTPServer) ListenAndServe(root string, port int) {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc(root+"/todos", h.todos)
+	mux.HandleFunc(root+"/todo", h.create)
 
 	http.ListenAndServe(":"+strconv.Itoa(port), mux)
+}
+
+func (h *HTTPServer) create(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	form := service.TodoForm{
+		Title: "title",
+	}
+	todo, err := h.service.Create(r.Context(), form)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	body := fmt.Sprintf("Todo: %v", todo)
+	io.WriteString(w, body)
 }
 
 func (h *HTTPServer) todos(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
 	}
 
 	todos, err := h.service.Todos(r.Context())
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 
 	body := fmt.Sprintf("Todos: %v", todos)
